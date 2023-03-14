e95502c31da7399dafba1fc013576fb66cc3a193
Ailec1 <cctropico16@gmail.com>
2023-03-13 20:03:00 -0400

    change

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -32,11 +32,9 @@
         </section>
       </header>
       <div id="text-zone">
-        <h2>Play!/h2>
+        <h2>Play!</h2>
         <h3>Click here to play</h3>
-          <form id="play">
-            <input type="submit"  id="play">Play></input>
-          </form>
+          
       </div>
       <footer>
         <nav>
@@ -61,122 +59,4 @@ export default {
 };
 </script>
   
-  <style lang="scss">
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  font-family: Century Gothic;
-}
-header,
-footer {
-  background-color: rgba(24, 2, 83, 0.6);
-}
-header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 2.5cm;
-  background-size: cover;
-  background-position: center;
-  font-style: oblique;
-}
 
-h1 {
-  font-size: 45px;
-}
-ul {
-  font-size: 15px;
-  list-style: none;
-  float: left;
-  display: flex;
-  margin-top: 35px;
-  margin-right: 10px;
-}
-
-ul li a {
-  text-decoration: none;
-  color: rgb(0, 0, 0);
-  text-transform: uppercase;
-  border: 2.5px solid transparent;
-  margin: 10px;
-  padding: 5px;
-  font-size: 25px;
-}
-
-ul li a:hover {
-  background: rgba(24, 2, 83, 0.6);
-  color: white;
-}
-
-#play {
-  margin-top: -20px;
-}
-
-#play a {
-  font-size: 40px;
-  border: 2px solid;
-  padding: 10px;
-  background: rgb(255, 255, 255);
-}
-
-#play a:hover {
-  transition: 0.6s ease;
-  background: rgba(24, 2, 83, 0.6);
-  color: white;
-}
-
-body {
-  background-attachment: fixed;
-  height: 100vh;
-  background-size: cover;
-  background-position: center;
-}
-
-#text-zone {
-  margin: 1.5cm;
-  height: 20cm;
-  background-color: rgb(214, 214, 214, 0.7);
-}
-
-footer {
-  height: 1.5cm;
-  background-size: cover;
-  background-position: center;
-  font-style: oblique;
-  height: 60px;
-  /* positionner le footer en bas de la page */
-  position: relative;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-size: c;
-  background-position: center;
-  text-decoration: none;
-  color: #ffffff;
-  margin-top: 5cm;
-  padding: 1px 10px;
-}
-
-footer a {
-  position: relative;
-  text-decoration: none;
-  color: rgb(0, 0, 0);
-  text-transform: uppercase;
-  float: right;
-  text-decoration: none;
-  color: rgb(0, 0, 0);
-  text-transform: uppercase;
-  border: 2.5px solid transparent;
-  padding: 0px;
-  font-size: 20px;
-}
-footer a:hover a {
-  transition: 0.6s ease;
-  background: rgba(24, 2, 83, 0.6);
-  color: white;
-  font-size: 40px;
-  border: 2px solid;
-  padding: 10px;
-}
-</style>
