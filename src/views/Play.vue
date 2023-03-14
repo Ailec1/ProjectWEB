827fa0d75974bddbb44cf9ed83d439d9e495be76
ArTranArt <arthur.tran@efrei.net>
2023-03-14 12:29:07 -0400

    Update Play.vue

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -30,6 +30,29 @@
       </header>
       <div id="text-zone">
         <h1>Play</h1>
+        <div class="ruban" id="ruban1">
+          <img class="pokemon-image" src="../assets/Shuffle001.png" alt="Description de l'image">
+          <div class="pokemon-text">
+            <h4 class="pokemon-name">Bulbasaur</h4>
+          <p class="pokemon-description">Innocent and carefree, Bulbasaur loves to wander around on its own. However, the problem is that it doesn't always realize the dangers around it and sometimes ends up getting lost in hazardous places, such as a volcano! Help it avoid the fire attacks of wild Pokémon! It's really hot and burning!</p>
+          </div>
+        </div>
+    
+        <div class="ruban" id="ruban4">
+          <img class="pokemon-image" src="../assets/Shuffle004.png" alt="Description de l'image">
+          <div class="pokemon-text">
+            <h4 class="pokemon-name">Charmander</h4>
+          <p class="pokemon-description">Charmander is a shy creature that doesn't like much. However, it's very popular among its peers who would love to give it hugs. With its pretty flaming tail and its tough-guy attitude, Charmander is a true CHAD in its neighborhood!</p>
+          </div>
+        </div>
+        
+        <div class="ruban" id="ruban7">
+          <img class="pokemon-image" src="../assets/Shuffle007.png" alt="Description de l'image">
+          <div class="pokemon-text">
+            <h4 class="pokemon-name">Squirtle</h4>
+          <p class="pokemon-description">Squirtle is quite carefree because it believes its shell is the strongest on the planet! But despite its tough-guy appearance, Squirtle is a cute creature that loves to eat bubbles! Splash!</p>
+          </div>
+        </div>
       </div>
       <footer>
         
