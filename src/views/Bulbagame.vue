9e20555388b251b8709fd2b2a316f2dcd68c8888
Ailec1 <cctropico16@gmail.com>
2023-03-23 13:44:01 -0400

    delete link to login and sign up

--- src/views/Bulbagame.vue
+++ src/views/Bulbagame.vue
@@ -13,10 +13,6 @@
                 <router-link class="site" to="pokedex">Pokedex</router-link>
               </li>
               <li><router-link class="site" to="score">Score</router-link></li>
-              <li><router-link class="site" to="login">Log In</router-link></li>
-              <li>
-                <router-link class="site" to="signup">Sign Up</router-link>
-              </li>
             </ul>
           </div>
         </section>
